7b3094edbaea56e2a3372e315fb9cad932bd4e3d
TTVTheAgregado <igorcauan036@gmail.com>
2023-11-29 09:56:22 -0400

    Testing

---
+++ app.py
@@ -0,0 +1,12 @@
+Nome = "Igor"
+idade = "16"
+gosto_de = "programar"
+
+
+
+
+print(f"Meu nome é {Nome} ")
+print(f"Gosta de {gosto_de} {Nome}")
+print(f"Eu tenho {idade} anos")
+print(f"E soiu top {idade}")
+print(f"E legal {gosto_de}")
