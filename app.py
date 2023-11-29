9d81d177b49d5ed034a47a27a52de16f601127fd
TTVTheAgregado <igorcauan036@gmail.com>
2023-11-29 10:04:09 -0400

    Calculations-Test

--- app.py
+++ app.py
@@ -1,12 +1,2 @@
-Nome = "Igor"
-idade = "16"
-gosto_de = "programar"
-
-
-
-
-print(f"Meu nome é {Nome} ")
-print(f"Gosta de {gosto_de} {Nome}")
-print(f"Eu tenho {idade} anos")
-print(f"E soiu top {idade}")
-print(f"E legal {gosto_de}")
+num1 = 5
+num2 = 3.5
